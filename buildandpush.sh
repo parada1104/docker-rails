f2219ba6529726383901317132687bc3a8b25a1b
parada1104 <57575053+parada1104@users.noreply.github.com>
2021-08-07 15:32:18 -0400

    Update buildandpush.sh

--- buildandpush.sh
+++ buildandpush.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-IMAGE_NAME="parada1104/buiaco-prod"
-BUILD_TARGET="prod-build"
+IMAGE_NAME="YOUR_IMAGE_TAG"
+BUILD_TARGET="YOUR_TARGET (dev-build/prod-build)"
 docker build -t $IMAGE_NAME . --target $BUILD_TARGET
 docker push $IMAGE_NAME
